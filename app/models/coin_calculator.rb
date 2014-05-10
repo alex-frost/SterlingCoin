d79526599798f056c52e558d6ba582bb8079fff9
Alex Frost <alex.frost.dev@gmail.com>
2014-05-10 09:07:52 +0100

    add coin_input_formatter class

--- app/models/coin_calculator.rb
+++ app/models/coin_calculator.rb
@@ -1,15 +1,6 @@
 class CoinCalculator
   def initialize(input_amount)
-    @input_amount = input_amount
-  end
-
-  def split_into_pounds_and_pence
-    remove_pounds_and_pence_signs.split(".")
-  end
-
-  def remove_pounds_and_pence_signs
-    striped = (@input_amount[0] == "£") ? @input_amount[1..-1] : @input_amount
-    (striped[-1] == "p") ? striped[0..-2] : striped
+    @coin_input_formatter = CoinInputFormatter.new(input_amount)
   end
 
 end
--- spec/models/coin_formatter_spec.rb
+++ spec/models/coin_formatter_spec.rb
@@ -1,6 +1,6 @@
-describe CoinCalculator do
+describe CoinInputFormatter do
   context "valid number with £ sign" do
-    subject {CoinCalculator.new("£4.47")}
+    subject {CoinInputFormatter.new("£4.47")}
 
     its(:remove_pounds_and_pence_signs) {should == "4.47"}
     its(:split_into_pounds_and_pence) {should == ["4","47"]}
