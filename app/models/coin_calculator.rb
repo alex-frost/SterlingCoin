fb697cd099d556a41fa118340e88b4a62f258895
Alex Frost <alex.frost.dev@gmail.com>
2014-05-10 09:01:21 +0100

    add splitting methods

--- app/models/coin_calculator.rb
+++ app/models/coin_calculator.rb
@@ -2,4 +2,14 @@ class CoinCalculator
   def initialize(input_amount)
     @input_amount = input_amount
   end
+
+  def split_into_pounds_and_pence
+    remove_pounds_and_pence_signs.split(".")
+  end
+
+  def remove_pounds_and_pence_signs
+    striped = (@input_amount[0] == "£") ? @input_amount[1..-1] : @input_amount
+    (striped[-1] == "p") ? striped[0..-2] : striped
+  end
+
 end
--- spec/models/coin_calculator_spec.rb
+++ spec/models/coin_calculator_spec.rb
@@ -1,5 +1,8 @@
 describe CoinCalculator do
-  subject {CoinCalculator.new("£4.47")}
+  context "valid number with £ sign" do
+    subject {CoinCalculator.new("£4.47")}
 
-  its(:numeric_value) {should == 4.47}
+    its(:remove_pounds_and_pence_signs) {should == "4.47"}
+    its(:split_into_pounds_and_pence) {should == ["4","47"]}
+  end
 end
