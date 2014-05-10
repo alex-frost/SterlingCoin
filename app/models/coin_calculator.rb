326f5b68bd56f1d04b1682763b1706b47952cb4c
Alex Frost <alex.frost.dev@gmail.com>
2014-05-10 11:50:08 +0100

    wip

--- app/models/coin_calculator.rb
+++ app/models/coin_calculator.rb
@@ -1,11 +1,13 @@
 class CoinCalculator
+  delegate :pounds, :pence, :valid?, to: :coins
+
   def initialize(input_amount)
     @input_amount = input_amount
   end
 
   def amount_in_sterling
-    if coins.valid?
-      {}
+    if valid?
+      convert(pounds, pound_coins).merge(convert(pence, pence_coins))
     else
       {}
     end
@@ -13,7 +15,27 @@ class CoinCalculator
 
   private
 
+  def convert(pound_or_pence, denomination)
+    remaining_coin = pound_or_pence
+
+    output = {}
+    denomination.each do |d|
+      num_in_denomination, remaining_coin = remaining_coin.divmod(d)
+      output[d] = num_in_denomination
+    end
+    output
+  end
+
   def coins
     @coins ||= CoinInputFormatter.new(@input_amount)
   end
+
+  def pound_coins
+    @pound_coins ||= [2,1]
+  end
+
+  def pence_coins
+    @pence_coins ||= [50,20,10,5,2,1]
+  end
+
 end
