0b9816dc52a00eb56b842c6bb22d8edbe92ffd36
Alex Frost <alex.frost.dev@gmail.com>
2014-05-10 08:54:16 +0100

    add create and only trust :amount in parmas

--- app/controllers/coin_calculator_controller.rb
+++ app/controllers/coin_calculator_controller.rb
@@ -2,4 +2,14 @@ class CoinCalculatorController < ApplicationController
   def new
 
   end
+
+  def create
+    CoinCalculator.new(coin_calculator_params)
+  end
+
+  private
+
+  def coin_calculator_params
+    params[:amount]
+  end
 end
