fffe5adc9053065664983446e0b79c604dad7b90
Alex Frost <alex.frost.dev@gmail.com>
2014-05-10 12:19:33 +0100

    add generic code to dsiplay flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_flash_class(name)
+    case name.to_s
+    when 'notice'
+      'info'
+    when 'error'
+      'danger'
+    else
+      name.to_s
+    end
+  end
 end
