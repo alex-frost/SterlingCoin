4641c4b5eb412440cde49f79c56fecb8fa57774f
Alex Frost <alex.frost.dev@gmail.com>
2014-05-10 08:30:11 +0100

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SterlingCoin::Application.config.secret_key_base = '80489d65b0ce5402da4bb87bc403ae63f48335f62599c070e451e1de3ff3909e93012fe648177886caefd51ea9412183ebf51d032ef0dc7921bd0a8e9beca270'
